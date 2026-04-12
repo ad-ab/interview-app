f502e92c4afce14705fd106797705cfa76741751
Adam <adam@Masina.localdomain>
2026-04-12 14:44:43 +0200

    fix(no-ticket): Create a TODO tile on pages that are not implemented yet

--- apps/web/src/routes/configuration/CoolingPage.tsx
+++ apps/web/src/routes/configuration/CoolingPage.tsx
@@ -1,7 +1,15 @@
+import PageTitle from "@/components/PageTitle";
+import { Stack, Tile } from "@carbon/react";
+import { useTranslation } from "react-i18next";
+
 export default function CoolingPage() {
+  const { t } = useTranslation();
+
   return (
-    <div>
-      <h1>Cooling</h1>
-    </div>
-  )
+    <Stack gap={1} className="tw-w-full">
+      <PageTitle>{t("config.cooling")}</PageTitle>
+
+      <Tile>TODO</Tile>
+    </Stack>
+  );
 }
--- apps/web/src/routes/configuration/PerformancePage.tsx
+++ apps/web/src/routes/configuration/PerformancePage.tsx
@@ -1,7 +1,15 @@
+import PageTitle from "@/components/PageTitle";
+import { Stack, Tile } from "@carbon/react";
+import { useTranslation } from "react-i18next";
+
 export default function PerformancePage() {
+  const { t } = useTranslation();
+
   return (
-    <div>
-      <h1>Performance</h1>
-    </div>
-  )
+    <Stack gap={1} className="tw-w-full">
+      <PageTitle>{t("config.performance")}</PageTitle>
+
+      <Tile>TODO</Tile>
+    </Stack>
+  );
 }
